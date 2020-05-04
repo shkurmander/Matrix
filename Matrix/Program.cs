009884780dee852ec595bd91a18091b1d4f7a809
Vladimir Berdnikov <gazetter@mail.ru>
2020-05-04 18:55:04 +0700

    GetMinor - успешно возвращает минорные матрицы меньшего порядка

--- Matrix/Program.cs
+++ Matrix/Program.cs
@@ -13,8 +13,16 @@ namespace Matrix
             Console.WriteLine("Введите порядок матрицы:");
             var n = Int32.Parse(Console.ReadLine());
             SqMatrix a = new SqMatrix(n);
-            //a.Input();
+            a.Input();
+            Console.WriteLine($"\nИсходная матрица:");
             a.Print();
+            for (int i = 0; i < a.GetN(); i++)
+            {
+                Console.WriteLine($"Минорная матрица #{i+1}") ;
+                a.GetMinor(a, i, a.GetN());
+            }
+            Console.ReadKey();
         }
+        
     }
 }
--- Matrix/SqMatrix.cs
+++ Matrix/SqMatrix.cs
@@ -11,18 +11,24 @@ namespace Matrix
         /// <summary>
         /// Массив-матрица
         /// </summary>
-        private int[,] Table  { get; set; }
+        private int[,] Table  {get; set; }
 
         /// <summary>
         /// Размерность матрицы-массива
         /// </summary>
         private int N { get; set; }
 
+
         public SqMatrix(int n)
         {
             this.N = n;
-            //Table = new int[N,N];
-            Table = new int[4, 4] { { 1, 2, 4, 3 }, { -6, 5, -13, 8 }, { 9, 5, 8, 1 },{ -1, 4, -3, 2 } };
+            Table = new int[N,N];
+            //Table = new int[4, 4] { { 1, 2, 4, 3 }, { -6, 5, -13, 8 }, { 9, 5, 8, 1 },{ -1, 4, -3, 2 } };
+        }
+
+        public int GetN()
+        {
+            return N;
         }
         public void Print()
         {
@@ -35,6 +41,7 @@ namespace Matrix
                 }
                 Console.WriteLine();
             }
+            Console.WriteLine();
         }
         public void Input()
         {
@@ -50,6 +57,38 @@ namespace Matrix
                 }                                
                 
             }
+        }
+
+        public SqMatrix GetMinor(SqMatrix array,int row, int n)
+        {
+            SqMatrix minor = new SqMatrix(n-1);
+            
+
+                int s = 0; int r = 0;
+                for (int i = 1; i < n; i++)
+                {
+                    
+                    int j = 0;
+                    do
+                    {
+                    if (j != row)
+                    {
+                        minor.Table[s, r] = array.Table[i, j];
+                        ++j; ++r;
+                    }
+                    else
+                    {
+                        ++j;
+                        continue;
+                    }
+
+                    } while (j<n);
+                    ++s; r = 0;
+                }
+                minor.Print();
+                return minor;
+
+        
         }
 
     }
